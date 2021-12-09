041c9101b0e2b303522b720bdec38cdcaacfe16c
onlis <aleksei@onl.is>
2021-12-09 20:06:41 +0500

    lesson done

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+const button = document.querySelector("#btn");
+const input = document.querySelector("#text");
+const square = document.querySelector("#square");
+const btn2 = document.querySelector("#e_btn");
+const range = document.querySelector("#range");
+const rangeSpan = document.querySelector("#range-span");
+const circle = document.querySelector("#circle");
 
+button.addEventListener("click", function () {
+  if (input.value.length > 0) {
+    square.style.backgroundColor = input.value;
+    input.value = "";
+  } else {
+    alert("Please enter value");
+  }
+});
 
+btn2.addEventListener("click", function () {
+  btn2.style.display = "none";
+});
 
-
-
+range.addEventListener("input", function () {
+  rangeSpan.textContent = range.value;
+  circle.style.height = range.value + "%";
+  circle.style.width = range.value + "%";
+});
